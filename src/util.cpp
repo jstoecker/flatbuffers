b640bfd6e4adc7de2999c757c638ed78d08387c0
Justin Stoecker <justoeck@microsoft.com>
2022-03-25 14:37:58 -0700

    gdk getenv

--- src/util.cpp
+++ src/util.cpp
@@ -245,7 +245,7 @@ bool ReadEnvironmentVariable(const char *var_name, std::string *_value) {
 #ifdef _MSC_VER
   __pragma(warning(disable : 4996));  // _CRT_SECURE_NO_WARNINGS
 #endif
-#ifdef WINAPI_FAMILY_PARITION(WINAPI_PARTITION_GAMES)
+#ifdef _GAMING_XBOX
   auto env_str = ::getenv(var_name);
 #else
   auto env_str = std::getenv(var_name);
